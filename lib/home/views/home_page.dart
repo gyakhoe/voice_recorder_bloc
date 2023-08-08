80e6da7ccadd5022c9bedded0c620d7f1b9a1340
gyakhoe <gyakhoe@gmail.com>
2023-08-08 19:47:55 +0545

    recording view added

--- lib/home/views/home_page.dart
+++ lib/home/views/home_page.dart
@@ -1,13 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:voice_recorder_bloc/recording/views/recording_view.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Recorder')),
-      body: const Center(child: Text('Home Page')),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(title: const Text('Recorder')),
+        body: Column(
+          children: [
+            //* First child is for the recroding view
+            const Expanded(
+              flex: 8,
+              child: RecordingView(),
+            ),
+            //* Second child is for recorder view
+            Expanded(
+              flex: 2,
+              child: Container(
+                color: Colors.amber,
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
